50607fdc18b4a5c2f898bc9ce35832d425b35204
HalaSalameh <haladaoud13@gmail.com>
2017-12-07 18:51:56 +0200

    h

--- dataBasep/src/database/DataBase.java
+++ dataBasep/src/database/DataBase.java
@@ -13,8 +13,7 @@ public class DataBase {
     public static void main(String[] args) {
             JPanel logIn  = new LogIn();
             JFrame f=new JFrame();
-            f.add(logIn);
-           
+            f.add(logIn);   
             f.pack();
             f.setSize(800,500);
             f.setVisible(true);
