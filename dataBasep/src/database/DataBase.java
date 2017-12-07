98b50f774b3a245dc26a2110f840afbee340296d
HalaSalameh <haladaoud13@gmail.com>
2017-12-07 19:00:07 +0200

    h

--- dataBasep/src/database/DataBase.java
+++ dataBasep/src/database/DataBase.java
@@ -1,6 +1,8 @@
 package database;
 
+import java.awt.Dimension;
 import java.awt.Frame;
+import java.awt.Toolkit;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -13,9 +15,15 @@ public class DataBase {
     public static void main(String[] args) {
             JPanel logIn  = new LogIn();
             JFrame f=new JFrame();
+           
             f.add(logIn);   
             f.pack();
-            f.setSize(800,500);
+             Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+            int height = screenSize.height;
+            int width = screenSize.width;
+            f.setSize(width/2, height/2);
+
+            f.setLocationRelativeTo(null);
             f.setVisible(true);
     }
     
