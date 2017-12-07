c15c12f5287a4aac8f885b67f6bde0b4e7f30914
HalaSalameh <haladaoud13@gmail.com>
2017-12-07 18:43:27 +0200

    su

--- dataBasep/src/database/DataBase.java
+++ dataBasep/src/database/DataBase.java
@@ -1,9 +1,6 @@
 
 package database;
 
-<<<<<<< HEAD
-
-=======
 import java.awt.Frame;
 import javax.swing.JPanel;
 
@@ -11,20 +8,20 @@ import javax.swing.JPanel;
  *
  * @author noorSh & halaS
  */
->>>>>>> 74f818f3082ede0e6872fd77c4e81a39cf9c41b2
+
 public class DataBase {
 
    
     public static void main(String[] args) {
-<<<<<<< HEAD
+
         
-=======
-          Frame f  = new Frame();
+
+            Frame f  = new Frame();
             JPanel logIn  = new LogIn();
             f.add(logIn);
             f.pack();
             f.setVisible(true);
->>>>>>> 74f818f3082ede0e6872fd77c4e81a39cf9c41b2
+
     }
     
 }
