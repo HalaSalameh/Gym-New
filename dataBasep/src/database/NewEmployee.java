439679e384862c08b0d5580e28dfa1e618bfbe9c
NoorShamasneh <noor.i.shamasneh@gmail.com>
2017-12-07 21:35:30 +0200

    Revert "Revert "jjjj""
    This reverts commit d7aea6941a0d8ccd893bd673fc33a68dddc900ed.

--- dataBasep/src/database/NewEmployee.java
+++ dataBasep/src/database/NewEmployee.java
@@ -67,6 +67,11 @@ public class NewEmployee extends javax.swing.JPanel {
 
         jButton3.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
         jButton3.setText("Exit");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
         add(jButton3);
         jButton3.setBounds(410, 610, 55, 23);
 
@@ -204,6 +209,11 @@ public class NewEmployee extends javax.swing.JPanel {
     private void jTextField3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField3ActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jTextField3ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+        DataBase.exitFrame();
+    }//GEN-LAST:event_jButton3ActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
